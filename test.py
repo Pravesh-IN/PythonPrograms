5e45949ad2f98471b1d747df117c2d40d336fdaa
Pravesh P <pk.mishra2388@gmail.com>
2022-08-02 09:47:52 +0100

    Adding a nef function intro in test.py

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 def greet(name, time='morning'):
     return f'Hello {name}, Good {time}'
 
+def intro(name, profession):
+    return f'My name is {name} and My profession is {profession}'
+
+
 print(greet('John','Evening'))
 
 wish=greet
-#del greet
 
-funct= [wish, greet, str.lower, str.capitalize]
+
+funct= [wish, greet, str.lower, str.capitalize, intro]
 
 for f in funct:
     print(f.__name__)
